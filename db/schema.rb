e91ae68d50604165beb29c07c478450fd47c2db2
Nicolas Hovart <kola.hov@gmail.com>
2017-12-01 13:43:04 +0100

    FC

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,10 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20140620105502) do
 
-  create_table "tasks", force: true do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "description", null: false
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description", null: false
     t.datetime "deadline"
   end
 
